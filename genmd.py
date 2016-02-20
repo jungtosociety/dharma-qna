2e6cf6341216f3402e56574983ffd6159982b490
Muae <muae.jungto@gmail.com>
2016-02-20 15:39:09 -0500

    database and generating script

---
+++ genmd.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+f = open('PROJECTS.md', 'w')
+c = sqlite3.connect('dharmaqna.db')
+
+f.write('## Published\n\n');
+f.write('| NO | TITLE         | XLS | PUBDATA | EN | FR | DE |\n')
+f.write('|----| ------------- |-----|---------|----|----|----|\n')
+
+for row in c.execute('SELECT v.vid,en.title,v.xlsfn,v.pubdate,en.fn,fr.fn,de.fn \
+                        FROM video v \
+                        LEFT OUTER JOIN en ON v.vid = en.vid \
+                        LEFT OUTER JOIN fr ON v.vid = fr.vid \
+                        LEFT OUTER JOIN de ON v.vid = de.vid \
+                       WHERE status=\'published\' \
+                       ORDER BY v.pubdate DESC \
+                         ' ):
+    vid     = row[0]
+    title   = row[1].encode('utf8')
+    xlsfn   = row[2]
+    pubdate = row[3]
+    fn_en   = row[4]
+    fn_fr   = row[5]
+    fn_de   = row[6]
+    # titlelink = "[#%s %s](sub/%s)" % (vid, title, vid)
+    nolink = "[%s](sub/%s)" % (vid,vid)
+    xlslink = "[![](img/excel.png)](sub/%s/%s)" % (vid,xlsfn)
+    enlink  = "[en](sub/%s/%s)" % (vid,fn_en) if fn_en is not None else ''
+    frlink = "[fr](sub/%s/%s)" % (vid,fn_fr) if fn_fr is not None else ''
+    delink = "[de](sub/%s/%s)" % (vid,fn_de) if fn_de is not None else ''
+    f.write("| %s | %s | %s | %s | %s | %s | %s |\n" % ( nolink, title, xlslink, pubdate, enlink, frlink, delink ))
+
+
+# print ' ![excel logo](/img/excel.png)'
+
